a3bcabbcd8adf3546d50c55adbc4b40d93d10c20
KarthickRavi594 <karthickravi594@gmail.com>
2018-06-25 00:43:15 +0530

    seperated with suitable files of extension

---
+++ script/popup.js
@@ -0,0 +1,6 @@
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    var lay = document.getElementById("layer");
+    popup.classList.toggle("show");
+    lay.classList.toggle("hide");
+}
